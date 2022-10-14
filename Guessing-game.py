1bd17bd79ab208c5593d1c801312c75b50c13b0e
Vincent <112147164+Vinny902@users.noreply.github.com>
2022-10-14 03:20:37 -0400

    Add files via upload

---
+++ Guessing-game.py
@@ -0,0 +1,45 @@
+from bdb import Breakpoint
+import random
+name = input("What is your name?\n")
+print("Hello", name, "welcome to my program")
+
+while True:
+    top_of_range = input("Type a number:\n")
+
+
+    if top_of_range.isdigit():
+        top_of_range = int(top_of_range)
+
+        if top_of_range <= 0:
+            print("Please type a number larger than 0 next time.")
+            continue
+    else:
+        print("Please type a number next time.")
+        continue
+    break
+
+random_number = random.randint(0, top_of_range)
+guesses = 0
+   
+
+while True:
+    guesses += 1
+    user_guess = input("Make a guess: \n")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+    else:
+        print("Please type a number next time.")
+        continue
+
+    if user_guess == random_number:
+        print("Correct, You got it right!")
+        break
+    
+    elif user_guess > random_number:
+        print("You were above the number!")
+    else:
+        print("You were below the number!")
+        
+
+print("You got it in", guesses, "guesses")
+
